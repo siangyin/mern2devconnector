89d64298719230e98a218466e44169ae281a2abb
Lee Siang Yin <siangyin916@gmail.com>
2021-12-23 20:02:25 +0800

    ⚙️ custom Auth Middleware & JWT ✅ console.log user db obj in terminal but  ❌ err: cannot get json send in postman

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
+// const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
+// const jwt = require("jsonwebtoken");
+// const config = require("config");
+// const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
 
-router.get("/", (req, res) => res.send(`auth route`));
+router.get("/", auth, async (req, res) => {
+	console.log(req.user.id);
+	try {
+		const userDB = await User.findOne({ _id: req.user.id }).select("-password");
+		console.log(userDB);
+		// console.log is working but json not showing
+		return res.status(200).json({ userDB });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send(`server error`);
+	}
+});
 
 module.exports = router;
