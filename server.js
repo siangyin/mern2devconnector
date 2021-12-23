2098014307430838ec0373e7d7b5dc485161c350
Lee Siang Yin <siangyin916@gmail.com>
2021-12-23 14:52:54 +0800

    🛣 Route files with express router

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 
 //connect DB
-connectDB()
+connectDB();
 
 app.get("/", (req, res) => {
 	res.send(`API running`);
 });
+
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/users"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => {
 	console.log(`server on ${PORT}`);
 });
