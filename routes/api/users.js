993a28c65380910539e11c33862b775fb3bb0988
Lee Siang Yin <siangyin916@gmail.com>
2021-12-23 18:49:40 +0800

    🔐 implementing jwt jsonwebtoken

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 
@@ -31,7 +33,9 @@ router.post(
 			// if user exists
 			let user = await User.findOne({ email });
 			if (user) {
-				return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+				return res
+					.status(400)
+					.json({ errors: [{ msg: "User already exists" }] });
 			}
 
 			// get user gravatar
@@ -49,7 +53,23 @@ router.post(
 			await user.save();
 
 			// return jsonwebtoken
-			res.send(`${user.name} user registered`);
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: "5 days" },
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+
+			
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send("Server error");
